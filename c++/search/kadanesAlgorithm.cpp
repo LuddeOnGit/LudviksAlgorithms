27ca18aa9b0a4b1aca23378d5061c2f2338cbeff
LuddeOnGit <hdguys11@gmail.com>
2020-03-16 23:05:02 +0100

    Added kadanesAlgorithm.cpp

---
+++ c++/search/kadanesAlgorithm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int array[] = {-2,2,5,-11,6};
+	int max_sum = array[0];
+	int current_sum = array[0];
+	int size_of_array = sizeof(array)/sizeof(array[0]);
+
+	for(int i = 1; i < size_of_array; i++)
+	{
+		current_sum += array[i];
+		if(array[i] > current_sum){current_sum = array[i];}
+		if(max_sum < current_sum){max_sum = current_sum;}
+	
+	}
+
+	cout << max_sum << endl;
+
+	return 0;
+
+}
