e96e36e3b8cf2d3a86520496d8d73a303ed08610
LuddeOnGit <hdguys11@gmail.com>
2020-03-16 23:20:53 +0100

    Added kadanesAlgorithm.py

---
+++ python/search/kadanesAlgorithm.py
@@ -0,0 +1,10 @@
+array = [-2,2,5,-11,6]
+max_sum = array[0]
+current_sum = array[0]
+
+for i in range(1,len(array)):
+    current_sum += array[i]
+    max_sum = current_sum if current_sum > max_sum else max_sum
+    current_sum = array[i] if array[i] > current_sum else current_sum
+
+print(max_sum)
